619fe7b0e57a239858a276b2f84a398e04dec06c
Ming Jian <mingjiannnn@gmail.com>
2026-03-20 00:32:02 +0800

    update chrome driver configurations

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DriverFactory {
@@ -15,10 +16,19 @@ public class DriverFactory {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-        options.setExperimentalOption("prefs", Map.of(
-                "credentials_enable_service", false,
-                "profile.password_manager_enabled", false));
+        options.setExperimentalOption("excludeSwitches",
+                Collections.singletonList("enable-automation"));
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+
+        options.setExperimentalOption("prefs", prefs);
+
+        options.addArguments("--disable-features=PasswordLeakDetection");
+
+        options.addArguments("--incognito");
 
         driver.set(new ChromeDriver(options));
 
